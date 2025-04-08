c420b99a42a2f02dab1f6486cec47c00da4f7d5b
Khai Ha <Khaiha265@gmail.com>
2025-04-08 09:55:44 -0700

    Add project files.

---
+++ wk1labtest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace wk1labtest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Divide_EightbyFour_EqualsTwo()
+        {
+            int a = 8;
+            int b = -4;
+            Assert.Equal(2, Divide(a, b));
+        }
+
+        public double Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
